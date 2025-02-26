104a617cbe03d802df00c602d6df9363237362d3
CreativeSTH <sth.frontend@gmail.com>
2025-02-26 15:49:16 -0500

    finish all commin soon

--- src/components/comingSoon/ComingSoon.jsx
+++ src/components/comingSoon/ComingSoon.jsx
@@ -1,9 +1,15 @@
 import "../comingSoon/comingSoon.css"
 import bailarina from "/bailarina.png"
+import logo from "/logo_Beementes.svg"
+import logo2 from "/beemente.svg"
 
 function ComingSoon() {
   return (
     <section className="container">
+        <div className="logo__card">
+          <img className="logo__card__a" src={logo} />
+          <img className="logo__card__b" src={logo2} />
+        </div>
         <img className="cs__card__logo" src={bailarina} alt="Logo de Beementes" />
         <article className="cs__card">
             <h1 className="cs__card__title">Coming Soon</h1>
